b5004652a706301f40f54d6a002b6ccd7837281f
Peabrain <peabrainc64@hotmail.com>
2017-06-06 13:15:09 +0200

    dvcxy

---
+++ workspace/UpdateDatabase/src/time.java
@@ -0,0 +1,14 @@
+//
+// Klasse enthält Daten für die Relationen 'getweetetam' und 'genutztam'
+//
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class time {
+	public List<Integer> getweetedam = new ArrayList();	// TweetID's zu einem bestimmten Timestamp
+	public List<String> genutztam = new ArrayList();	// Hashtag zu einem bestimmten Timestamp
+	public time()
+	{
+	}
+}
